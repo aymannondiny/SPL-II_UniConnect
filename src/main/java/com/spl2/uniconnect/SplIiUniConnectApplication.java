28ca635718d8bca57aa30a11553db35c41d9ede4
aymannondiny <ayman@iut-dhaka.edu>
2026-05-12 22:56:34 +0600

    Add Spring Boot initial setup with Maven and DB config

---
+++ src/main/java/com/spl2/uniconnect/SplIiUniConnectApplication.java
@@ -0,0 +1,13 @@
+package com.spl2.uniconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplIiUniConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplIiUniConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spl2/uniconnect/SplIiUniConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spl2.uniconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplIiUniConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
